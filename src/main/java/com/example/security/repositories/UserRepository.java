2deb918ae04cae12a62a4716f74d463d1325f497
Paulo Henrique <varelapaulo852@gmail.com>
2026-07-16 16:59:49 -0300

    Creating UserRepository

---
+++ src/main/java/com/example/security/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.security.repositories;
+
+import com.example.security.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+}
